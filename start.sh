d6f9f6fe6634d00554d99270482a3453a0c95259
Sabith3938 <82069764+Sabith3938@users.noreply.github.com>
2023-07-19 19:52:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Sabith3938/DQ_BOT_V2 /DQ_BOT_V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /DQ_BOT_V2
 fi
-cd /DQTheFileDonorBot
+cd /DQ_BOT_V2
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting KOTHTHA DQ...."
 python3 bot.py
