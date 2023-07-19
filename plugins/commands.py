e638bb70866255a1e05f96ff4c9b2a6b33bceccd
Sabith3938 <82069764+Sabith3938@users.noreply.github.com>
2023-07-19 20:00:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -106,7 +106,7 @@ async def start(client, message):
                     InlineKeyboardButton('〄 Hᴇʟᴘ 〄', callback_data='help'),
                     InlineKeyboardButton('⍟ Aʙᴏᴜᴛ ⍟', callback_data='about')
                 ],[
-                    InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
+                    InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK),
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
